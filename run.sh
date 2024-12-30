cc4a3b67a8de38dbd695a6064d47de88d98538e4
sec-report <130388298+sec-report@users.noreply.github.com>
2024-12-30 20:19:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
+downloadSelf(){
+    if [ ! -f "run.sh" ]; then
+        wget https://raw.githubusercontent.com/sec-report/SecAutoBan/main/run.sh -O run.sh
+        chmod +x run.sh
+    fi
+}
 
 downloadDockerCompose(){
-     if [ ! -f "docker-compose.yml" ]; then
-         wget https://raw.githubusercontent.com/sec-report/SecAutoBan/main/docker-compose.yml
-     fi
+    if [ ! -f "docker-compose.yml" ]; then
+        wget https://raw.githubusercontent.com/sec-report/SecAutoBan/main/docker-compose.yml -O docker-compose.yml
+    fi
 }
 
 createPassword(){
     if [ ! -f ".env" ]; then
-        echo "clickhouse_password=$(uuidgen |sed 's/-//g')" > .env
+        echo "mysql_password=$(uuidgen |sed 's/-//g')" > .env
+    fi
+}
+
+exec() {
+    if [ "$1" = "changeUserPassword" ]
+    then
+        docker compose exec sec-report /sec_report $1 $2 $3 $4 $5
     fi
 }
 
@@ -28,6 +41,10 @@ update(){
          rm docker-compose.yml
          downloadDockerCompose
     fi
+    if [ -f "run.sh" ]; then
+         rm run.sh
+         downloadSelf
+    fi
     docker compose pull
 }
 
@@ -38,6 +55,10 @@ then
 elif [ "$1" = "update" ]
 then
     update
+    exit
+elif [ "$1" = "exec" ]
+then
+    exec $2 $3 $4 $5 $6
     exit
 fi
 
