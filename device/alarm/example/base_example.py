b7d59cfacf9cb5a3984a6a400a89880da6daaa6d
sec-report <130388298+sec-report@users.noreply.github.com>
2025-03-08 18:42:00 +0800

    Update base_example.py

--- device/alarm/example/base_example.py
+++ device/alarm/example/base_example.py
@@ -1,11 +1,11 @@
 import time
 from SecAutoBan import SecAutoBan
 
-# 实现解析设备log函数，最后调用send_alarm_ip("ip", "备注")向服务器发送告警IP
+# 实现解析设备log函数，最后调用send_alarm_ip("攻击ip", "受害IP","备注")向服务器发送告警IP
 def alarm_analysis(ws_client):
     while True:
         time.sleep(1)
-        ws_client.send_alarm("127.1.0.3", "NMAP 扫描 127.0.0.1")
+        ws_client.send_alarm("127.1.0.3", "127.0.0.1","NMAP 扫描")
 
 
 if __name__ == "__main__":
